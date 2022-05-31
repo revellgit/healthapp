f9fd319b7ece9eef1847db079b2abf307ad928e8
revellgit <mrmattrevell@gmail.com>
2022-05-31 23:41:22 +1200

    Add new icons for menu card options. Add new activities.

---
+++ app/src/main/java/com/example/healthapp/MainActivity7.kt
@@ -0,0 +1,13 @@
+// Information - MainActivity7
+
+package com.example.healthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main7)
+    }
+}
---
+++ app/src/main/java/com/example/healthapp/MainActivity8.kt
@@ -0,0 +1,13 @@
+// About - MainActivity8
+
+package com.example.healthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main8)
+    }
+}
