c3ba3bb3c3147d2480555eec9d874c91938b56ec
revellgit <mrmattrevell@gmail.com>
2022-05-31 23:49:24 +1200

    Add id for cards to other features. Add click listeners to menu for navigating to each feature.

--- app/src/main/java/com/example/healthapp/MainActivity2.kt
+++ app/src/main/java/com/example/healthapp/MainActivity2.kt
@@ -19,6 +19,10 @@ class MainActivity2 : AppCompatActivity() {
 
         val btn1 = findViewById<CardView>(R.id.emergency_card)
         val btn2 = findViewById<CardView>(R.id.affirmations_card)
+        val btn3 = findViewById<CardView>(R.id.sentiment_card)
+        val btn4 = findViewById<CardView>(R.id.breathe_card)
+        val btn5 = findViewById<CardView>(R.id.information_card)
+        val btn6 = findViewById<CardView>(R.id.about_card)
 
         btn1.setOnClickListener {
             val intent = Intent(this, MainActivity3::class.java)
@@ -27,6 +31,26 @@ class MainActivity2 : AppCompatActivity() {
 
         btn2.setOnClickListener {
             val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
+
+        btn3.setOnClickListener {
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+        }
+
+        btn4.setOnClickListener {
+            val intent = Intent(this, MainActivity6::class.java)
+            startActivity(intent)
+        }
+
+        btn5.setOnClickListener {
+            val intent = Intent(this, MainActivity7::class.java)
+            startActivity(intent)
+        }
+
+        btn6.setOnClickListener {
+            val intent = Intent(this, MainActivity8::class.java)
             startActivity(intent)
         }
 
