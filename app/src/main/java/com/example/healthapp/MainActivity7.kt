22891bd9fc8cf944356e667e88b3038d7d3261df
revellgit <mrmattrevell@gmail.com>
2022-06-05 22:30:57 +1200

    Add logo button to info screen that takes user to a webpage in browser

--- app/src/main/java/com/example/healthapp/MainActivity7.kt
+++ app/src/main/java/com/example/healthapp/MainActivity7.kt
@@ -2,12 +2,28 @@
 
 package com.example.healthapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main7)
+
+        val url: String = "https://www.facebook.com/calm/"
+
+        val calm_btn = findViewById<ImageView>(R.id.calm_logo)
+
+
+        calm_btn.setOnClickListener {
+            val uri = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+
     }
 }
